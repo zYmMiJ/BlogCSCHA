0223169be52eeefbd66030e7d7b9a2853e7738e6
zYmMiJ <avaejimmy@gmail.com>
2020-01-19 04:32:05 +1100

    add video and style

--- Blog/Models/Post.cs
+++ Blog/Models/Post.cs
@@ -11,6 +11,8 @@ namespace Blog.Models
         public string Title { get; set; } = "";
         public string Body { get; set; } = "";
         public string Image { get; set; } = "";
+        public string Video { get; set; } = "";
+
 
         public string Description { get; set; }= "";
         public string Tags { get; set; } = "";
